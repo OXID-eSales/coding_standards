1fc7061e66912f750a202aed427864bbe8c901d8
Tobias Matthaiou <tm@solutiondrive.de>
2015-01-21 11:57:41 +0100

    Better Error Messages, and Code reforming.

--- Sniffs/NamingConventions/ValidVariableNameSniff.php
+++ Sniffs/NamingConventions/ValidVariableNameSniff.php
@@ -4,11 +4,11 @@
  *
  * PHP version 5
  *
- * @category  PHP
- * @package   PHP_CodeSniffer
- * @author    Tobias Matthaiou
- * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
- * @link      http://pear.php.net/package/PHP_CodeSniffer
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Tobias Matthaiou <matthaiou@solutiondrive.de>
+ * @license  https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
  */
 
 /**
@@ -28,13 +28,15 @@ class Oxid_Sniffs_NamingConventions_ValidVariableNameSniff extends Zend_Sniffs_N
 {
 
     /**
-     * A list of hungarian Notation
-     * @var string
+     * A list of hungarian Notation.
+     *
+     * @var string is used in a RegEx-Pattern
      */
     private $_hungarianNotation = "(a|bl|o|s|i|f|d|h|fn|m|is|my|db|fld)";
 
     /**
      * A list of reserved var, then its ok.
+     *
      * @var array
      */
     protected $ignoreVaribaleNames = array(
@@ -136,23 +138,24 @@ class Oxid_Sniffs_NamingConventions_ValidVariableNameSniff extends Zend_Sniffs_N
     /**
      * Check the Variable Name of Hungarian Notation.
      *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the
-     *                                        stack passed in $tokens.
-     * @param string               $varName   The name of Variable
-     * @param string               $code      A violation code unique to the sniff message.
+     * @param PHP_CodeSniffer_File $phpcsFile           The file being scanned.
+     * @param int                  $stackPtr            The position of the current token in the
+     *                                                  stack passed in $tokens.
+     * @param string               $varName             The name of Variable
+     * @param string               $code                A violation code unique to the sniff message.
+     * @param int|bool             $recordsWarningLevel Set the phpcf-Records as warning.
+     *                                                  The severity level for this warning.
+     *                                                  (Optional) default is Error-Records
      *
      * @return void
      */
     protected function checkHungarianNotation(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $varName, $code, $recordsWarningLevel = false)
     {
         if (!preg_match("/^_?{$this->_hungarianNotation}[A-Z]/", $varName)) {
-            $error = 'Variable "%s" start not with a Type character';
-            if ($code == 'SubVarNotTypeName')
-                $error = 'Object variable "%s" start not with a Type character';
+            $error = $this->getErrorMsg($code);
             $data  = array($varName);
             if ($recordsWarningLevel !== false) {
-                $phpcsFile->addWarning($error, $stackPtr, $code, $data, $recordsWarningLevel);
+                $phpcsFile->addWarningOnLine($error, $stackPtr, $code, $data, $recordsWarningLevel);
             } else {
                 $phpcsFile->addError($error, $stackPtr, $code, $data);
             }
@@ -196,4 +199,29 @@ class Oxid_Sniffs_NamingConventions_ValidVariableNameSniff extends Zend_Sniffs_N
 
         $this->checkHungarianNotation($phpcsFile, $stackPtr+2, $varName, 'SubVarNotTypeName', 0);
     }//end subVariablename
+
+    /**
+     * Get the Error Messages of a unique sniff
+     *
+     * @param string $code A violation code unique to the sniff message.
+     *
+     * @return string
+     */
+    protected function getErrorMsg($code)
+    {
+        $errorMsg = 'Variable "%s" start not with a Type character';
+        switch ($code) {
+            case 'SubVarNotTypeName':
+                $errorMsg = 'Object variable "%s" start not with a Type character';
+                break;
+            case 'MemberVarNotTypeName':
+                $errorMsg = 'Class Member variable "%s" start not with a Type character';
+                break;
+            case 'StringVarNotTypeName':
+                $errorMsg = 'Variable in String "%s" start not with a Type character';
+                break;
+        }
+
+        return $errorMsg;
+    }//end getErrorMsg
 }//end class
